4d6f02dbd8428fe76d1a683d68c1018578dd7f8d
Mitsubishimas <kisluhin.jura87@gmail.com>
2026-09-01 05:49:30 +0000

    v1.9.9: Version fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fameli.budget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 77
-        versionName = "1.9.8"
+        versionCode = 78
+        versionName = "1.9.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "YANDEX_CLIENT_ID", "\"your_yandex_client_id\"")
     }
