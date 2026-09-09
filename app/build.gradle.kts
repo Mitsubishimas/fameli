7102f7aeb598533b72d1d9d726abc65d812df05f
Mitsubishimas <kisluhin.jura87@gmail.com>
2026-09-09 06:50:10 +0000

    v1.10.25: repeat_type sync from API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fameli.budget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
-        versionName = "1.10.24"
+        versionCode = 104
+        versionName = "1.10.25"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "YANDEX_CLIENT_ID", "\"your_yandex_client_id\"")
     }
